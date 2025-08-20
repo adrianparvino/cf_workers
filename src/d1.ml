3b0126d2e6954c13deebcaeec1914c7c144230c3
Adrian Parvin Ouano <me@adrianpar.vin>
2025-08-20 13:55:06 +0800

    refactor: add type-safe D1 argument binding

--- src/d1.ml
+++ src/d1.ml
@@ -3,10 +3,22 @@ type bound_prepared_statement
 type prepared_statement
 type 'a results = { results : 'a }
 
+module Bind = struct
+  type t
+
+  external unsafeCast : 'a -> t = "%identity"
+
+  let number : int -> t = unsafeCast
+  let string : string -> t = unsafeCast
+
+  let null : ('a -> t) -> 'a option -> t =
+   fun f -> fun x -> x |> Option.map f |> Js.Null.fromOption |> unsafeCast
+end
+
 external run : bound_prepared_statement -> 'a results Js.Promise.t = "run"
 [@@mel.send]
 
-external bind : prepared_statement -> 'a array -> bound_prepared_statement
+external bind : prepared_statement -> Bind.t array -> bound_prepared_statement
   = "bind"
 [@@mel.send] [@@mel.variadic]
 
