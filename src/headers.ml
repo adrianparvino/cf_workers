8d3ea1429d657c2a69b0826564e582ff1d8d6424
Adrian Parvin Ouano <me@adrianpar.vin>
2025-11-03 03:39:24 +0800

    fix: headers should be nullable not option

--- src/headers.ml
+++ src/headers.ml
@@ -1,5 +1,8 @@
 type t
 
 external empty : unit -> t = "Headers" [@@mel.new]
-external get : string -> (t [@mel.this]) -> string option = "get" [@@mel.send]
-external set : string -> string -> (t [@mel.this]) -> unit = "set" [@@mel.send]
+
+external get : string -> (t[@mel.this]) -> string Js.Nullable.t = "get"
+[@@mel.send]
+
+external set : string -> string -> (t[@mel.this]) -> unit = "set" [@@mel.send]
